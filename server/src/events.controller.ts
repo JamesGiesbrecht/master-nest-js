61b26e181994bdd3f42b087eb3daaad734af9360
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-10-11 09:42:18 -0500

    Returning some dummy data and status codes where applicable

--- server/src/events.controller.ts
+++ server/src/events.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Patch,
   Post,
@@ -11,17 +12,29 @@ import {
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAll() {}
+  findAll() {
+    return [
+      { id: 1, name: 'My Birthday' },
+      { id: 2, name: 'Christmas' },
+    ];
+  }
   @Get(':id')
   findOne(@Param('id') id) {
-    return id;
+    return { id: 1, name: 'My Birthday' };
   }
   @Post()
   create(@Body() input) {
+    // return created resource
     return input;
   }
   @Patch(':id')
-  update(@Param('id') id, @Body() input) {}
+  update(@Param('id') id, @Body() input) {
+    // Return updated resource
+    return input;
+  }
   @Delete(':id')
-  remove(@Param('id') id) {}
+  @HttpCode(204)
+  remove(@Param('id') id) {
+    return;
+  }
 }
