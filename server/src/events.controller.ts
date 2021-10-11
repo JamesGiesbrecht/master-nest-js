1c0eaf43348c29629afb2c3ec062248c01cda763
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-10-11 09:44:44 -0500

    Creating a class to type Post body

--- server/src/events.controller.ts
+++ server/src/events.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { CreateEventDto } from './create-event.dto';
 
 @Controller('/events')
 export class EventsController {
@@ -23,7 +24,7 @@ export class EventsController {
     return { id: 1, name: 'My Birthday' };
   }
   @Post()
-  create(@Body() input) {
+  create(@Body() input: CreateEventDto) {
     // return created resource
     return input;
   }
