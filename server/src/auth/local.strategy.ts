58882626ae22167aec1f075f578410559fdb4afa
James Giesbrecht <giesbrechtjames@gmail.com>
2021-10-13 22:09:36 -0500

    Checking supplied password against hashed password

--- server/src/auth/local.strategy.ts
+++ server/src/auth/local.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { Strategy } from 'passport-local';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -24,7 +25,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
       throw new UnauthorizedException();
     }
 
-    if (password !== user.password) {
+    if (!(await bcrypt.compare(password, user.password))) {
       // Password would be encrypted
       this.logger.debug(`Invalid credentials for ${username}`);
       throw new UnauthorizedException();
