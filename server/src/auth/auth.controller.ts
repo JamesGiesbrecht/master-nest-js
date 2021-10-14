3d00df8de65cd0f25fdf793426eebff6614fa13d
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-10-13 20:55:57 -0500

    Generating a JWT token and returning it to the user

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('login')
   @UseGuards(AuthGuard('local'))
-  async login(@Request() request) {
+  async login(@Request() req) {
     return {
-      userId: request.user.id,
-      token: 'the token will go here',
+      userId: req.user.id,
+      token: this.authService.getTokenForUser(req.user),
     };
   }
 }
