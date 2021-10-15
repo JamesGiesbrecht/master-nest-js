47fbaf6af9d80562ae99b6ce6473e9af6ca731ac
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-10-14 21:01:43 -0500

    Adding controllers to get the events organized by a user

--- server/src/events/events.module.ts
+++ server/src/events/events.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Attendee } from './attendee.entity';
+import { AttendeesService } from './attendee.service';
+import { EventAttendeesController } from './event-attendees.controller';
 import { Event } from './event.entity';
+import { EventsOrganizedByUserController } from './events-organized-by-user.controller';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
@@ -10,7 +13,11 @@ import { EventsService } from './events.service';
     // Injecting a repository for the Event class
     TypeOrmModule.forFeature([Event, Attendee]),
   ],
-  controllers: [EventsController],
-  providers: [EventsService],
+  controllers: [
+    EventsController,
+    EventsOrganizedByUserController,
+    EventAttendeesController,
+  ],
+  providers: [EventsService, AttendeesService],
 })
 export class EventsModule {}
--- server/src/events/events.service.ts
+++ server/src/events/events.service.ts
@@ -4,7 +4,7 @@ import { User } from 'src/auth/user.entity';
 import { paginate, PaginateOptions } from 'src/pagination/paginator';
 import { DeleteResult, Repository } from 'typeorm';
 import { AttendeeAnswerEnum } from './attendee-answer.enum';
-import { Event } from './event.entity';
+import { Event, PaginatedEvents } from './event.entity';
 import { CreateEventDto } from './inputs/create-event.dto';
 import { UpdateEventDto } from './inputs/update-event.dto';
 import { ListEvents, WhenEventFilter } from './list.events';
@@ -85,7 +85,7 @@ export class EventsService {
   public async getEventsWithAtttendeeCountFilteredPagniated(
     filter: ListEvents,
     paginateOptions: PaginateOptions,
-  ) {
+  ): Promise<PaginatedEvents> {
     return await paginate(
       await this.getEventsWithAttendeeCountFiltered(filter),
       paginateOptions,
@@ -128,5 +128,21 @@ export class EventsService {
       .delete()
       .where('id = :id', { id })
       .execute();
+  }
+
+  public async getEventsOrganizedByUserIdPaginated(
+    userId: number,
+    paginateOptions: PaginateOptions,
+  ): Promise<PaginatedEvents> {
+    return await paginate<Event>(
+      this.getEventsOrganizedByUserIdQuery(userId),
+      paginateOptions,
+    );
+  }
+
+  private getEventsOrganizedByUserIdQuery(userId: number) {
+    return this.getEventsBaseQuery().where('e.organizerId = :userId', {
+      userId,
+    });
   }
 }
